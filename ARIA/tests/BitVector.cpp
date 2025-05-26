1f8a2ec73b8bcd1d6302f8b383faa4f2d71ac318
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-26 23:47:10 +0800

    .

---
+++ ARIA/tests/BitVector.cpp
@@ -0,0 +1,14 @@
+#include "ARIA/BitVector.h"
+
+#include <boost/ut.hpp>
+
+using namespace ARIA;
+using namespace boost::ut;
+
+namespace {
+
+suite<"BitVector"> _ = [] {
+  // TODO: Implement this.
+};
+
+} // namespace
