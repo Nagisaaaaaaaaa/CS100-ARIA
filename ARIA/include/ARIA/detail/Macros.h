d69b1efe8add6de65c6f601a74574cf59b40f0a8
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-27 14:17:56 +0800

    .

--- ARIA/include/ARIA/detail/Macros.h
+++ ARIA/include/ARIA/detail/Macros.h
@@ -8,6 +8,7 @@
 //
 //
 //
+#include <cassert>
 #include <iostream>
 
 // Compilers.
--- ARIA/tests/detail/Macros.cpp
+++ ARIA/tests/detail/Macros.cpp
@@ -7,7 +7,50 @@ using namespace boost::ut;
 namespace {
 
 suite<"Macros"> _ = [] {
-  // TODO: Implement this.
+  "Compilers"_test = [] {
+    static_assert((ARIA_CLANG ? 1 : 0) + (ARIA_ICC ? 1 : 0) + (ARIA_MSVC ? 1 : 0) + (ARIA_GCC ? 1 : 0) == 1);
+  };
+
+  //
+  //
+  //
+  "Misc"_test = [] {
+    static_assert(ARIA_NUM_OF(a) == 1);
+    static_assert(ARIA_NUM_OF(a, b) == 2);
+    static_assert(ARIA_NUM_OF(a, b, c) == 3);
+
+    static_assert(ARIA_CONCAT(233, 466) == 233466);
+    static_assert(ARIA_ANON(999) == 99920);
+
+    static_assert(ARIA_COND(1, 5, 6) == 5);
+    static_assert(ARIA_COND(0, 5, 6) == 6);
+    static_assert(ARIA_COND(true, 5, 6) == 5);
+    static_assert(ARIA_COND(false, 5, 6) == 6);
+
+    std::string str;
+    ARIA_IF(1, str = "1");
+    expect(str == "1");
+    ARIA_IF(0, str = "0");
+    expect(str == "0");
+    ARIA_IF(true, str = "true");
+    expect(str == "true");
+    ARIA_IF(false, str = "false");
+    expect(str == "true");
+  };
+
+  //
+  //
+  //
+  "Assertions"_test = [] {
+    auto unimplemented = [] { ARIA_UNIMPLEMENTED; };
+
+    int a = 1;
+    ARIA_ASSERT(a == 1);
+    ARIA_ASSERT(a == 1, "`a` does not equal to 1");
+
+    ARIA_EXPECT(a == 1);
+    ARIA_EXPECT(a == 1, "`a` does not equal to 1");
+  };
 };
 
 } // namespace
